62a67173408c668eff648b5bd6c03233f99ac957
Angel <mroa7623@gmail.com>
2023-07-26 16:31:54 -0500

    Creation of display_board function

--- board.py
+++ board.py
@@ -1,5 +1,5 @@
 import numpy
-
+import pygame
 board = [['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
          ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
          [None, None, None, None, None, None, None, None],
@@ -8,3 +8,21 @@ board = [['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
          [None, None, None, None, None, None, None, None],
          ['WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP'],
          ['WR', 'WN', 'WB', 'WQ', 'WK', 'WB', 'WN', 'WR']]
+
+def display_board(screen, SCREEN_WIDTH, SCREEN_HEIGHT, CELL_SPACE, COLOR_BOARD1, COLOR_BOARD2):
+
+    # Variable COLOR to identify when the board must show "COLOR_BOARD1" or "COLOR_BOARD2"
+    COLOR = 0
+
+    # Creation of the table board
+    # For clicle of the matrix
+    for i in range(0, SCREEN_WIDTH, CELL_SPACE):
+        for j in range(0, SCREEN_HEIGHT, CELL_SPACE):
+            if COLOR % 2 == 0:
+                # Creation of the color cel 1
+                pygame.draw.rect(screen, COLOR_BOARD1, [i, j, CELL_SPACE, CELL_SPACE])
+            else:
+                # Creation of the color cel 2
+                pygame.draw.rect(screen, COLOR_BOARD2, [i, j, CELL_SPACE, CELL_SPACE])
+            COLOR += 1
+        COLOR += 1
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pygame import *
-from board import board
+from board import *
 import pygame
 import math
 
@@ -35,21 +35,8 @@ while run:
             SCEL_X, SCEL_Y = math.trunc(x/CELL_SPACE), math.trunc(y/CELL_SPACE)
             print(SCEL_X, SCEL_Y)
 
-# Variable COLOR to identify when the board must show "COLOR_BOARD1" or "COLOR_BOARD2"
-    COLOR=0
-# Creation of the table board
-    # For clicle of the matrix
-    for i in range(0, SCREEN_WIDTH, CELL_SPACE):
-        for j in range(0, SCREEN_HEIGHT, CELL_SPACE):
-            if COLOR % 2 == 0 :
-                # Creation of the color cel 1
-                pygame.draw.rect(screen, COLOR_BOARD1, [i, j, CELL_SPACE, CELL_SPACE])
-            else:
-                # Creation of the color cel 2
-                pygame.draw.rect(screen, COLOR_BOARD2, [i, j, CELL_SPACE, CELL_SPACE])
-            COLOR += 1
-        COLOR += 1
 
+    display_board(screen, SCREEN_WIDTH, SCREEN_HEIGHT, CELL_SPACE, COLOR_BOARD1, COLOR_BOARD2)
     # Show the board
     screen.blit(wp, (0, 0))
     pygame.display.flip()
