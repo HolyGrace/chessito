9542e134f7fdd29234a59c2ff13471b8d25c3957
Angel <mroa7623@gmail.com>
2023-07-26 12:32:24 -0500

    Creation of the board and click coordenate identification

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from pygame import *
 import pygame
+import math
 
 pygame.init()
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 800
+
+COLOR_BOARD1 = (0, 0, 0)
+COLOR_BOARD2 = (255, 255, 255)
+
+CELL_SPACE = int(SCREEN_WIDTH/8)
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -13,4 +21,34 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        #Recolector of the mouse click information
+        if event.type == MOUSEBUTTONDOWN and event.button == 1:
+            x, y = mouse.get_pos()
+            print(x, y)
+
+            #SCEL_X, SCEL_Y =(math.trunc(x/CELL_SPACE), math.trunc(y/CELL_SPACE))
+            #print(SCEL_X, SCEL_Y)
+            SCEL_X, SCEL_Y =x/CELL_SPACE, y/CELL_SPACE
+            print(SCEL_X, SCEL_Y)
+
+#variable COLOR to identify when the board must show "COLOR_BOARD1" or "COLOR_BOARD2"
+    COLOR=0
+
+##Creation of the table board
+    ##For clicle of the matrix
+    for i in range(0, SCREEN_WIDTH, CELL_SPACE):
+        for j in range(0, SCREEN_HEIGHT, CELL_SPACE):
+            if COLOR % 2 == 0 :
+                #Creation of the color cel 1
+                pygame.draw.rect(screen, COLOR_BOARD1, [i, j, CELL_SPACE, CELL_SPACE])
+            else:
+                # Creation of the color cel 2
+                pygame.draw.rect(screen, COLOR_BOARD2, [i, j, CELL_SPACE, CELL_SPACE])
+            COLOR += 1
+        COLOR += 1
+
+    #Show the board
+    pygame.display.flip()
+
+
 pygame.quit()
